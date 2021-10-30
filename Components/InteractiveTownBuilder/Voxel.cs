437f9aa6025062ee6b76b9499680444c4d8dcbaa
Mathias Sønderskov Schaltz <sonderskovmathias@gmail.com>
2021-10-30 12:46:28 +0200

    Create Voxel.cs

---
+++ Components/InteractiveTownBuilder/Voxel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveTownBuilder
+{
+    struct Voxel
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Voxel(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
