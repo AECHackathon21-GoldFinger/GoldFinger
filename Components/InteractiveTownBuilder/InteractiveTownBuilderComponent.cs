cdf954740750caee489fa52db93968782ad6e3fc
Povl Filip Sonne-Frederiksen <pfs@linkarkitektur.dk>
2021-10-30 13:47:52 +0200

    Creating the ground plane

--- Components/InteractiveTownBuilder/InteractiveTownBuilderComponent.cs
+++ Components/InteractiveTownBuilder/InteractiveTownBuilderComponent.cs
@@ -9,6 +9,9 @@ namespace InteractiveTownBuilder
     public class InteractiveTownBuilderComponent : GH_Component
     {
 
+        private Box SolutionSpace = Box.Empty;
+        Box[] SolutionArray = null;
+
         public InteractiveTownBuilderComponent()
           : base("InteractiveTownBuilder", "ITB",
             "This component enables interactive town building in Grasshopper",
@@ -19,23 +22,101 @@ namespace InteractiveTownBuilder
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddBoxParameter("Solution Space", "SS", "A bounding box of the solution space to consider", GH_ParamAccess.item, Box.Empty );
+            pManager.AddBoxParameter("Solution Space", "SS", "A bounding box of the solution space to consider", GH_ParamAccess.item, Box.Empty);
+            pManager.AddNumberParameter("Size", "s", "The size of a voxel meter", GH_ParamAccess.item, 3);
         }
 
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             pManager.AddPointParameter("Slots", "S", "The slots that monoceros will use for its solution", GH_ParamAccess.list);
+            pManager.AddBoxParameter("Boxes", "B", "The slotes represented as boxes", GH_ParamAccess.list);
         }
+
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            Box solutio_space = Box.Empty;
+            double size = 3;
+
+            DA.GetData("Solution Space", ref solutio_space);
+            DA.GetData("Size", ref size);
+
+
+            bool updated = CheckInput(solutio_space, ref SolutionSpace);
+
+
+            if (updated)
+                SolutionArray = SubdivideBox(SolutionSpace, size);
+
+            DA.SetDataList("Boxes", SolutionArray);
+
+
         }
 
 
         protected override System.Drawing.Bitmap Icon => null;
 
+        public override GH_Exposure Exposure => GH_Exposure.primary;
 
         public override Guid ComponentGuid => new Guid("BF47A037-8EC2-4370-A6C8-16148149E47F");
+
+
+        // Check if the inputes have changed  
+        private bool CheckInput(Box solutionSpace_new, ref Box solutionSpace_existing)
+        {
+            // Compare hashes
+            if (true)
+            {
+                solutionSpace_existing = solutionSpace_new;
+                return true;
+            }
+            return false;
+        }
+
+
+        // Subdivide the the bounding box
+        private Box[] SubdivideBox(Box box, double cell_size)
+        {
+            var x_axis = box.X;
+            var y_axis = box.Y;
+            var groundHeight = box.Z.Min;
+
+            Plane plane = new Plane(new Point3d(x_axis.Min, y_axis.Min, groundHeight), Vector3d.XAxis, Vector3d.YAxis );
+
+
+            // Later check if geometry already exists and no ground plane needs to be created
+            if (true)
+                return ConstructBaseplane(plane, cell_size, box.X, box.Y);
+
+        }
+
+        private Box[] ConstructBaseplane(Plane plane, double size, Interval X, Interval Y) 
+        {
+            var coutInX = ((int)Math.Round(X.Length / size));
+            var coutInY = ((int)Math.Round(Y.Length / size));
+
+
+            Interval x = new Interval(X.Min, X.Min + size);
+            Interval y = new Interval(X.Min, X.Min + size);
+            Interval z = new Interval(plane.OriginZ , plane.OriginZ - size);
+
+
+            Box[] results = new Box[coutInX * coutInY];
+
+
+            for (int i = 0; i < (coutInX * coutInY); i += coutInX) 
+            {
+                for (int j = 0; j < coutInX; ++j) 
+                {
+                    results[i + j] = new Box(plane, x , y, z);
+                    x += size;
+                }
+                y += size;
+                x = new Interval(X.Min, X.Min + size);
+            }
+
+            return results;
+        }
     }
 }
