ada6ee890f31263b9a8e72a771dd9dc67558ba06
Mathias Sønderskov Schaltz <sonderskovmathias@gmail.com>
2021-10-30 12:47:56 +0200

    Update Voxel.cs

--- Components/InteractiveTownBuilder/Voxel.cs
+++ Components/InteractiveTownBuilder/Voxel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace InteractiveTownBuilder
 {
-    struct Voxel
+    public struct Voxel
     {
 
         public int X { get; set; }
